415e322daccd7bdb560ab8be63b1b377b38d7abc
Geordie <geordie.s.moore@gmail.com>
2021-09-02 12:59:23 -0400

    api proxy / same-site cookie fix

---
+++ pages/api/proxy/[...slug].ts
@@ -0,0 +1,23 @@
+import { createProxyMiddleware } from 'http-proxy-middleware'
+
+// Create proxy instance outside of request handler function to avoid unnecessary re-creation
+const apiProxy = createProxyMiddleware({
+	target: 'https://recipe-node.herokuapp.com',
+	changeOrigin: true,
+	pathRewrite: { [`^/api/proxy`]: '/api' },
+	secure: false,
+})
+
+export default function (req, res) {
+	apiProxy(req, res, result => {
+		if (result instanceof Error) {
+			throw result
+		}
+
+		throw new Error(
+			`Request '${req.url}' is not proxied! We should never reach here!`
+		)
+	})
+}
+
+export const config = { api: { externalResolver: true, bodyParser: false } }
