95966de5d2e931ad821f269c560d565daade79b9
Geordie <geordie.s.moore@gmail.com>
2021-10-20 12:15:47 -0400

    Temporarily removed recipe submission

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -18,7 +18,7 @@ export const Nav: React.FC = () => {
 			<StyledNavButtonsWrapper>
 				{session && (
 					<>
-						<RecipeSubmitModal />
+						{/* <RecipeSubmitModal /> */}
 						<UserMenu />
 					</>
 				)}
